f4241b38d719ad81d4162046b54a8669bd9c8983
mstar95 <m.star@wp.pl>
2019-01-11 23:00:10 +0100

    publikacje

--- academics/gen_publication.py
+++ academics/gen_publication.py
@@ -1,19 +1,30 @@
 from habanero import Crossref
 
+import csv
+import json
 
+def safeGetTitle(x):
+    return x[0] if x else None
 
 with open('inventors_w_aliases.csv',"r") as csvfile:
     csv_inventors = csv.reader(csvfile, delimiter=',')
-    inventors_names_w_aliases  = list(csv_inventors)
+    data  = list(csv_inventors)
 
+aliases = list(set([x[1] for x in data[2:]]))
+aliases.sort()
 
-
-def get_man_
 cr = Crossref(mailto = "jedr.ka@gmail.com")
-x = cr.works(query_author = "")
-
-x['message']
-x['message']['total-results']
-x['message']['items']
+publications = dict()
+counter = 0
+for alias in aliases:
+    counter = counter + 1
+    print(alias, counter)
+    res = cr.works(query_author = alias)
+    publications[alias] = list(filter(None, [ safeGetTitle(x.get('title')) for x in res['message']['items'] ]))
+    if counter % 5:
+        with open('publikacjeDir/pub' + str(counter / 5) + '.json',"w") as jsonfile:
+            json.dump(publications, jsonfile)
+    
+    publications = dict()
 
 
