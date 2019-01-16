bf320e73a9f1844f1809f9b94fc900e1edfe9384
mstar95 <m.star@wp.pl>
2019-01-16 23:05:46 +0100

    roznice_w_patetnach

---
+++ academics/roznice_w_patentach.py
@@ -0,0 +1,47 @@
+import csv
+
+
+with open('inventors_w_aliases.csv',"r") as csvfile:
+    csv_inventors = csv.reader(csvfile, delimiter=',')
+    data  = list(csv_inventors)
+    aliases = list(set([x[1] for x in data[2:]]))
+
+with open('ludzie_liczba_patentow_normalized.csv',"r") as csvfile:
+    csv_patents = csv.reader(csvfile, delimiter=',')
+    patents = list(csv_patents)
+
+with open('publication_coworking.csv',"r") as csvfile:
+    csv_coworking = csv.reader(csvfile, delimiter=',')
+    coworking = list(csv_coworking)
+
+
+aliases.sort()
+
+result = []
+
+for alias in aliases:
+    res = list(filter(lambda p: p[0] == alias, patents))
+    if not res:
+        continue
+    patentNr = res[0][1]
+    coworkers = list(filter(lambda p: p[0] == alias, coworking))
+    finalValue = 0
+    coworkersCounter = 0
+    for coworker in coworkers:
+        name = coworker[1]
+        if name == alias:
+            continue
+        res = list(filter(lambda p: p[0] == name, patents))
+        if not res:
+            continue
+        value = int(coworker[2]) * int(res[0][1])
+        finalValue = finalValue + value
+        coworkersCounter = coworkersCounter + int(coworker[2])
+    if coworkersCounter != 0:
+        result.append((alias, patentNr, finalValue / coworkersCounter))
+        
+with open('roznice_w_patentach.csv',"w") as csvfile:
+    writer = csv.writer(csvfile,)
+    writer.writerow(("inventor","patents","coworkers_patents"))
+    subs = result
+    writer.writerows(subs)
